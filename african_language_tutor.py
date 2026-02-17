70584afeea558455659f73e0d00ddf6cd2d0d699
neema719 <grace.ngari@shinraitechnologies.io>
2026-02-17 11:43:22 +0300

    Fix create_retrieval_chain import path

--- african_language_tutor.py
+++ african_language_tutor.py
@@ -3,7 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.documents import Document
